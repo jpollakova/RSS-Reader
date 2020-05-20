817b0063a97aaa68642a26d094525c71ef6e2532
jpollakova <janepollakova@gmail.com>
2020-05-20 22:57:53 +0200

    trying some magic

--- rssreader/wsgi.py
+++ rssreader/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('DJANGO_ENV') == 'production':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rssreader.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rssreader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rssreader.settings')
 
 application = get_wsgi_application()
